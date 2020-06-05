e13a5ef58334180b5a2c85c57db2870c5fa00129
Zhilei Han <hzl17@mails.tsinghua.edu.cn>
2020-06-06 01:47:48 +0800

    remove unnecessary specs

--- verif/llvm.rkt
+++ verif/llvm.rkt
@@ -74,8 +74,6 @@
       (verify-llvm-refinement spec-default_free_pages implementation:@default_free_pages (list (make-bv64) (make-bv64))))
     (test-case+ "default_nr_free_pages LLVM"
       (verify-llvm-refinement spec-default_nr_free_pages implementation:@default_nr_free_pages (list)))
-    (test-case+ "step-consistency spec-default_free_pages LLVM"
-      (verify-step-consistency spec-default_free_pages (list (make-bv64) (make-bv64))))
     (test-case+ "nr_free_preservation spec-default_alloc_pages LLVM"
       (verify_nr_free_preservation spec-default_alloc_pages (list (make-bv64))))
     (test-case+ "nr_free_preservation spec-default_init_memmap LLVM"
